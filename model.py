51f5308e03df8eac60cabe5b4f87a0cc5e1bfe58
Orven E. Llantos <orven.llantos@gmail.com>
2015-10-03 00:04:33 +0800

    [NEW!] Added Model.

---
+++ model.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+import os
+
+class DBconn:
+    def __init__(self):
+        engine = create_engine("postgresql://flask:flasker@127.0.0.1:5432/flaskdb", echo=False)
+        self.conn = engine.connect()
+        self.trans = self.conn.begin()
+
+    def getcursor(self):
+        cursor = self.conn.connection.cursor()
+        return cursor
+
+    def dbcommit(self):
+        self.trans.commit()
+
+
