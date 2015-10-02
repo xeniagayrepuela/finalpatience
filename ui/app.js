cabaf2d5fbc653b0c6f98dd8714a5db2660b6aff
Orven E. Llantos <orven.llantos@gmail.com>
2015-10-03 01:34:49 +0800

    [NEW!] Created function to respond when button is clicked on the UI.

--- ui/app.js
+++ ui/app.js
@@ -1,14 +1,33 @@
+function rowtask(description, done, id, title)
+{
+   return '<div class="col-lg-12">' +
+          '<h4>' + id + "&nbsp;&nbsp;" + title + '</h4>' +
+          '<p>'+description+' </br> Status: '+done+'</p> </div>'; 
+}
+
+function loadtasks()
+{
+
 $.ajax({
     		url: 'http://127.0.0.1:5000/tasks',
     		type:"GET",
     		dataType: "json",
     		success: function(resp) {
-
+				$("#tasks").html("");
 				if (resp.status  == 'ok') {
-					console.log(resp);
+				   for (i = 0; i < resp.count; i++)
+                                  {
+                                       description = resp.entries[i].description;
+                                       done = resp.entries[i].done;
+                                       id = resp.entries[i].id;
+                                       title = resp.entries[i].title;
+                                       $("#tasks").append(rowtask(description, done, id, title));
+                                       
+	                          }
 				} else
 				{
-					console.log(resp.message);
+                                       $("#tasks").html("");
+					alert(resp.message);
 				}
     		},
     		error: function (e) {
@@ -19,3 +38,4 @@ $.ajax({
                         "Basic " + btoa("ako:akolagini"));
    			}
 		}); 
+}
