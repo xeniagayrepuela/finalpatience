d734ff4b3a20fce31ac78a28621fb977586f87bf
Orven E. Llantos <orven.llantos@gmail.com>
2015-10-02 23:52:36 +0800

    [NEW!] Insert new record stored proc.

--- progress.sql
+++ progress.sql
@@ -4,3 +4,27 @@ create table tasks (
    description text,
    done boolean
 );
+
+create or replace function newtask(par_id int8, par_title  text, par_description text, par_done boolean) returns text as
+$$
+  declare
+    loc_id text;
+    loc_res text;
+  begin
+     select into loc_id id from tasks where id = par_id;
+     if loc_id isnull then
+
+       insert into tasks (id, title, description, done) values (par_id, par_title, par_description, par_done);
+       loc_res = 'OK';
+
+     else
+       loc_res = 'ID EXISTED';  
+     end if;
+     return loc_res;
+  end;
+$$
+ language 'plpgsql';
+
+--select newtask(1, 'Buy Groceries','Milk, Cheese, Pizza, Fruit, Tylenol', false); 
+--select newtask(2, 'Learn Python','Need to find a good Python tutorial on the web', false); 
+
